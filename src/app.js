c655bec95ab75d904b09b3237f167323694abed7
Josh Perez <josh@goatslacker.com>
2013-07-22 15:18:57 -0700

    Allow use of legacy fixmyjs

--- src/app.js
+++ src/app.js
@@ -8,7 +8,15 @@
   }
 
   Editor.prototype.hint = function() {
-    this.editor.setValue(fixMyJS.fix(this.editor.getValue(), Options.options))
+    var code = this.editor.getValue()
+    if ($('input.legacy')[0].checked) {
+      JSHINT.jshint(code, Options.options)
+      this.editor.setValue(
+        fixMyJS(JSHINT.jshint.data(), code, Options.options).run())
+    } else {
+      this.editor.setValue(
+        fixMyJS.fix(this.editor.getValue(), Options.options))
+    }
   };
 
   Editor.getOptions = function() {
@@ -17,18 +25,17 @@
 
   var Scrim = {
     scrim: (function() {
-      var el, val;
-      el = document.createElement("div");
+      var el = document.createElement('div');
       document.body.appendChild(el);
-      val = $(el);
+      var val = $(el);
       val.css({
-        position: "fixed",
+        position: 'fixed',
         top: 0,
         left: 0,
-        background: "rgba(0, 0, 0, 0.8)",
-        zIndex: "9000",
-        width: "1920px",
-        height: "1080px"
+        background: 'rgba(0, 0, 0, 0.8)',
+        zIndex: '9000',
+        width: '1920px',
+        height: '1080px'
       }).hide();
       return val;
     }()),
@@ -61,7 +68,7 @@
       debug: false,
       immed: true,
       indent: 4,
-      indentpref: "spaces",
+      indentpref: 'spaces',
       lastsemic: false,
       laxbreak: true,
       maxerr: 500,
@@ -85,18 +92,18 @@
 
     show: function() {
       Scrim.show();
-      $("section.options").addClass("open");
-      return setTimeout((function() {
-        if (Scrim.isShowing) return $("span.close").addClass("visible");
+      $('section.options').addClass('open');
+      setTimeout((function() {
+        Scrim.isShowing && $('span.close').addClass('visible');
       }), 1000);
     },
 
     hide: function() {
-      $("span.close").removeClass("visible");
-      return setTimeout((function() {
+      $('span.close').removeClass('visible');
+      setTimeout((function() {
         if (Scrim.isShowing) {
-          $("section.options").removeClass("open");
-          return Scrim.hide();
+          $('section.options').removeClass('open');
+          Scrim.hide();
         }
       }), 200);
     }
@@ -119,7 +126,7 @@
 
   Dialog.close = function (id) {
     if (Dialog.openDialog) {
-      $(id).removeClass("bounceIn").addClass("bounceOut");
+      $(id).removeClass('bounceIn').addClass('bounceOut');
     }
     clearTimeout(Dialog.autoHideTimer);
     setTimeout(function() {
@@ -132,55 +139,62 @@
     if (Dialog.openDialog) {
       Dialog.close(Dialog.openDialog);
     }
-    $(id).removeClass("bounceOut").addClass("bounceIn").show();
+    $(id).removeClass('bounceOut').addClass('bounceIn').show();
     Dialog.autoHideTimer = setTimeout(Dialog.close.bind(Dialog, id), 7500);
     Dialog.openDialog = id;
   }
 
-  var Legacy = new Dialog("aside#legacy_dialog")
-  var About = new Dialog("aside#about_dialog")
+  var Legacy = new Dialog('aside#legacy_dialog')
+  var About = new Dialog('aside#about_dialog')
 
   $.domReady(function() {
     var env;
     env = new Editor();
-    $("section.options input").each(function(el) {
-      return $(el).on("click", function(event) {
-        var name;
-        name = event.target.name;
+    $('section.options input').each(function(el) {
+      $(el).on('click', function(event) {
+        var name = event.target.name;
         switch (name) {
-          case "indentpref":
+          case 'indentpref':
             return Options.setOption(name, event.target.value);
-          case "indent":
+          case 'indent':
             break;
           default:
             return Options.setOption(name, event.target.checked);
         }
       });
     });
-    $("section.options input.optText").on("change", (function(event) {
-      return Options.setOption(event.target.name, Number(event.target.value));
+
+    $('section.options input.optText').on('change', (function(event) {
+      Options.setOption(event.target.name, Number(event.target.value));
     }));
-    $("span.close").on("click", function() {
-      return Options.hide();
+
+    $('span.close').on('click', function() {
+      Options.hide();
     });
-    $("#options").on("click", function() {
-      return Options.toggle();
+
+    $('#options').on('click', function() {
+      Options.toggle();
     });
-    $("button").on("click", (function() {
-      return env.hint();
+
+    $('button').on('click', (function() {
+      env.hint();
     }));
-    $("#about").on("click", function() {
-      return About.open();
+
+    $('#about').on('click', function() {
+      About.open();
     });
-    $("#legacy").on("click", function() {
-      return Legacy.open();
+
+    $('#legacy').on('click', function() {
+      Legacy.open();
     });
-    $("aside#about_dialog").on("click", function() {
-      return About.close();
-    }).hide().addClass("animated");
-    $("aside#legacy_dialog").on("click", function() {
-      return Legacy.close();
-    }).hide().addClass("animated");
+
+    $('aside#about_dialog').on('click', function() {
+      About.close();
+    }).hide().addClass('animated');
+
+    $('aside#legacy_dialog').on('click', function() {
+      Legacy.close();
+    }).hide().addClass('animated');
   });
 
 }).call(this);
